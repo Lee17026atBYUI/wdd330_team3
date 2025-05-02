71ee93b5527eebe87524677d3011287def99908b
Lee17026atBYUI <leetim@byui.edu>
2025-05-02 13:28:36 -0600

    change localStorage to use arrays so we can stack items

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -11,7 +11,19 @@ export function getLocalStorage(key) {
 }
 // save data to local storage
 export function setLocalStorage(key, data) {
-  localStorage.setItem(key, JSON.stringify(data));
+	// check if we need to save what we already have in localStorage
+	var currentStorage = localStorage.getItem(key);
+	var newStorage;
+	if (currentStorage === null) {
+		// nothing in storage for this key, so just save as array of one
+		newStorage = JSON.stringify([data]);
+	} else {
+		// we have stuff in storage for this key (as an array) so add to it
+		var currentStorageArray = JSON.parse(currentStorage);
+		currentStorageArray.push(data);
+		newStorage = JSON.stringify(currentStorageArray);
+	}
+	localStorage.setItem(key, newStorage);
 }
 // set a listener for both touchend and click
 export function setClick(selector, callback) {
