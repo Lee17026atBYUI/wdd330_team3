6b6c24d5dcafa4103cd19fe00f869969ee096975
Lee17026atBYUI <leetim@byui.edu>
2025-05-09 12:57:47 -0600

    fix empty cart bug

--- src/js/cart.js
+++ src/js/cart.js
@@ -2,8 +2,10 @@ import { getLocalStorage } from "./utils.mjs";
 
 function renderCartContents() {
   const cartItems = getLocalStorage("so-cart");
-  const htmlItems = cartItems.map((item) => cartItemTemplate(item));
-  document.querySelector(".product-list").innerHTML = htmlItems.join("");
+  const emptyCartHtml = `<h3>Empty Cart. It is so sad and lonely here. Please buy a tent to make it warm and cozy.</h3>`;
+  // use optional chaining to check if this bad boy is empty
+  const htmlItems = cartItems?.map((item) => cartItemTemplate(item));
+  document.querySelector(".product-list").innerHTML = htmlItems ? htmlItems.join("") : emptyCartHtml;
 }
 
 function cartItemTemplate(item) {
